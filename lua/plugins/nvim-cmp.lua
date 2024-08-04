263093ee925f8d999d67b1dcfd5703ce2604c61d
Guillermo Rojas <apemangr@gmail.com>
2024-08-03 21:14:05 -0400

    Correct Luasnip jump with TAB and backward jump with S-TAB

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,19 +2,21 @@ return {
     "hrsh7th/nvim-cmp",
     dependencies = {
         "hrsh7th/cmp-buffer",
+        "saadparwaiz1/cmp_luasnip",
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            build = "make install_jsregexp",
+        },
     },
     config = function()
         local cmp = require'cmp'
-
+        local luasnip = require'luasnip'
         cmp.setup({
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
-                    --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
                     require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                    -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                    -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
                 end,
             },
             formatting = {
@@ -34,12 +36,30 @@ return {
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
                 ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
+                { name = 'luasnip'},
                 { name = 'obsidian' },
-                { name = 'codeium' }, -- For vsnip users.
-                -- { name = 'snippy' }, -- For snippy users.
+                { name = 'codeium' },
             }, {
                 { name = 'buffer' },
             })
