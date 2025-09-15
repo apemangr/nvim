4ca4617bb2bee48f419d24cdaf43a30c16a8e33b
Guillermo Rojas <apemangr@gmail.com>
2025-09-15 01:05:33 -0300

    clean: Limpieza autocms.lua

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -24,15 +24,11 @@ autocmd("TextYankPost", {
 	group = highlight_group,
 })
 
-
 local ignore_filetypes = { 'neo-tree', 'NvimTree_1', 'NvimTree' }
 local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
 
-local augroup =
-    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
-
 vim.api.nvim_create_autocmd('WinEnter', {
-    group = augroup,
+    group = augroup("FocusDisable", { clear = true }),
     callback = function(_)
         if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
         then
@@ -45,7 +41,7 @@ vim.api.nvim_create_autocmd('WinEnter', {
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-    group = augroup,
+    group = augroup("FocusDisable", { clear = true }),
     callback = function(_)
         if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
             vim.b.focus_disable = true
--- snippets/lua.lua
+++ snippets/lua.lua
@@ -34,3 +34,4 @@ return {
     i(0),  -- cursor final
   }),
 }
+
